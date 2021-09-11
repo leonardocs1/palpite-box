4e3fae693fc2c9055e31c7518ecae2a10fec0722
Leonardo Costa Santos <leonardo.costa.santos.si@gmail.com>
2021-09-11 17:43:21 -0300

    Conectando a planilha ao projeto

--- pages/api/get-promo.js
+++ pages/api/get-promo.js
@@ -1,7 +1,30 @@
+import { GoogleSpreadsheet } from 'google-spreadsheet'
+import credentials from '../../credentials.json'
+
+const doc = new GoogleSpreadsheet('1g6MW1mkgV_hmpTlf6cwjyVBzsD-Yk6LfKaElmFULABI')
 
 export default async (req, res) => {
-  res.end(JSON.stringify({
-    showCoupon: true,
-    message: 'Coupon message'
-  }))
+
+  try {
+    await doc.useServiceAccountAuth(credentials)
+    await doc.loadInfo()
+
+    const sheet = doc.sheetsByIndex[2]
+    await sheet.loadCells('A2:B2')
+
+    const mostrarPromocaoCell = sheet.getCell(1, 0)
+
+    const textoCell = sheet.getCell(1, 1)
+    console.log(textoCell.value)
+
+    res.end(JSON.stringify({
+      showCoupon: mostrarPromocaoCell.value === 'VERDADEIRO',
+      message: textoCell.value
+    }))
+  } catch (error) {
+    res.end(JSON.stringify({
+      showCoupon: false,
+      message: ''
+    }))
+  }
 }
--- pages/api/save.js
+++ pages/api/save.js
@@ -1,5 +1,25 @@
+import { GoogleSpreadsheet } from 'google-spreadsheet'
+import credentials from '../../credentials.json'
+
+const doc = new GoogleSpreadsheet('1g6MW1mkgV_hmpTlf6cwjyVBzsD-Yk6LfKaElmFULABI')
 
 export default async (req, res) => {
-  console.log(JSON.parse(req.body))
-  res.end(req.body)
+  try {
+    await doc.useServiceAccountAuth(credentials)
+    await doc.loadInfo()
+    const sheet = doc.sheetsByIndex[1]
+    const data = JSON.parse(req.body)
+    // Nome	Email	Whatsapp	Cupom	Promo
+    await sheet.addRow({
+      Nome: data.Nome,
+      Email: data.Email,
+      Whatsapp: data.Whatsapp,
+      Cupom: 'aaa111',
+      Promo: 'asdfasdf'
+    })
+    res.end(req.body)
+  } catch (err) {
+    console.log(err)
+    res.end('error')
+  }
 }
