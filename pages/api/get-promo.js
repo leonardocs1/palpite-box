b6c637835268bc389f9231dd9c07526c7490b24b
Leonardo Costa Santos <leonardo.costa.santos.si@gmail.com>
2021-09-11 15:35:24 -0300

    Pegando dados da planilha - useSWR e APIs no Nextjs

---
+++ pages/api/get-promo.js
@@ -0,0 +1,7 @@
+
+export default async (req, res) => {
+  res.end(JSON.stringify({
+    showCoupon: true,
+    message: 'Coupon message'
+  }))
+}
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import Link from 'next/link'
+import useSWR from 'swr'
+
+const fetcher = (...args) => fetch(...args).then(res => res.json())
 
 const Index = () => {
+  const { data, error } = useSWR('/api/get-promo', fetcher)
   return (
     <div>
       <p className='mt-12 text-center'>
@@ -13,9 +17,12 @@ const Index = () => {
           <a className='bg-blue-400 px-12 py-4 font-bold rounded-lg shadow-lg hover:shadow'>Dar opinião, ou sugestão</a>
         </Link>
       </div>
-      <p className='my-12 text-center'>
-        Mensagem de desconto.
-      </p>
+      {!data && <p>Carregando...</p>}
+      {!erro && data && data.showCoupon &&
+        <p className='my-12 text-center'>
+          {data.message}
+        </p>
+      }
     </div>
   )
 }
